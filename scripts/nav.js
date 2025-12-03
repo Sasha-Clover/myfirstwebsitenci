f55a3d9e831bdecef0a84f79af40f7b3b61681c6
Sasha-Clover <136914635+Sasha-Clover@users.noreply.github.com>
2025-12-03 18:49:01 +0000

    Add files via upload

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,22 +1,30 @@
 // all worked on navbar development
 // Tristan, Alex, Derek, Andrei
 var navbar = `
-		<nav class="navbar navbar-expand-lg" data-bs-theme="dark">
+		<nav class="navbar navbar-expand-lg ag_navbar" role="navigation">
 			<div class="container-fluid">
-				<a class="navbar-brand"  href="index.html">Group G</a>
+				<!-- branding -->
+				<a class="navbar-brand"  href="index.html">Group G |</a>
+				<!-- login menu -->
+				<!-- <button class="ag_login_icon" type="button"> -->
+				<!-- 	<span class="">Log in</span> -->
+				<!-- </button> -->
+				<!-- hamburger menu -->
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
 
 				<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-					<a class="nav-link active navBtn" aria-current="page" href="index.html">Home</a>
-					<a class="nav-link navBtn"  href="news.html">News</a>
-					<a class="nav-link navBtn"  href="uisceire.html">UisceIRE</a>
-					<a class="nav-link navBtn"  href="sources.html">Sources</a>
-					<a class="nav-link navBtn"  href="conservation.html">Conservation</a>
-					<a class="nav-link navBtn"  href="findus.html">Find Us</a>
-					<a class="nav-link navBtn"  href="contact.html">Contact Us</a>
-					<a class="nav-link navBtn"  href="info.html">Info</a>
+					<!-- buttons in compact form -->
+					<a class="nav-link navBtn" href="index.html">Home</a>
+					<a class="nav-link navBtn" href="news.html">News</a>
+					<a class="nav-link navBtn" href="uisceire.html">UisceIRE</a>
+					<a class="nav-link navBtn" href="sources.html">Sources</a>
+					<a class="nav-link navBtn" href="conservation.html">Conservation</a>
+					<a class="nav-link navBtn" href="findus.html">Find Us</a>
+					<a class="nav-link navBtn" href="contact.html">Contact Us</a>
+					<a class="nav-link navBtn" href="info.html">Info</a>
+					<button class="nav-link navBtn ag_loginBtn" onclick="pop_up();" id="ag_loginBtn">Log in</button>
 				</div>
 			</div>
 		</nav>`;
